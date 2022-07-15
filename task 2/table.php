866938567761595de9a5f79da6f386f6159c06f6
mahdy-maker <mostafamahdy938@gmail.com>
2022-07-15 23:19:14 +0200

    second task

---
+++ task 2/table.php
@@ -0,0 +1,53 @@
+<?php
+// dynamic table
+// dynamic rows //4 
+// dynamic columns // 4
+// check if gender of user == m ==> male // 1
+// check if gender of user == f ==> female // 1
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ],
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'menna',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+    ],  
+];
+
+?>
